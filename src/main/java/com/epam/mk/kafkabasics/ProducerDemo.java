8ec05440aee1951b1e220699beebeff0f70984dc
marta kostyk <martakostyk@op.pl>
2020-01-28 11:40:10 +0100

    implement producer, remove default test class, fix .gitignore

---
+++ src/main/java/com/epam/mk/kafkabasics/ProducerDemo.java
@@ -0,0 +1,40 @@
+package com.epam.mk.kafkabasics;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * Hello world!
+ */
+public class ProducerDemo {
+
+    private static final String BOOTSTRAP_SERVERS = "127.0.01:9092";
+
+    public static void main(String[] args) {
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
+
+        // send data
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
